ce44de9121a8e1481eca6f5961dfface2f720a3f
DMKuldeep <tkuldeep271@gmail.com>
2023-09-17 13:39:58 +0530

    updation

--- logics.js
+++ logics.js
@@ -60,7 +60,6 @@ const products = [
     price: 280,
     url: "./images/choc14.jpg",
   },
-  // Add the rest of your products here
 ];
 const carts = [];
 let TotalAmount = 0;
@@ -71,7 +70,7 @@ function removeCart(uniqueId) {
   const index = carts.findIndex((obj) => obj.uniqueId === uniqueId);
 
   if (index !== -1) {
-    carts.splice(index, 1); // Remove one element at the found index
+    carts.splice(index, 1); 
     if (carts.length === 7) {
       error.innerHTML = "";
     }
@@ -86,39 +85,30 @@ function addTableDta() {
     cartList.removeChild(cartList.firstChild);
   }
   carts.forEach((cart) => {
-    // Assuming you have a reference to the cart list element
 
-    // Create a new table row
     const tableRow = document.createElement("tr");
 
-    // Create table cells (td) for the columns you want in the row
     const productNameCell = document.createElement("td");
     const productPriceCell = document.createElement("td");
     const removeButtonCell = document.createElement("td");
 
-    // Add content to the table cells (you can replace these with actual product data)
     productNameCell.textContent = cart.name;
     productPriceCell.textContent = `₹ ${cart.price}`;
 
-    // Create a remove button and add a click event listener to remove the row when clicked
     const removeButton = document.createElement("button");
     removeButton.textContent = "Remove";
     removeButton.addEventListener("click", function () {
-      // Remove the table row when the remove button is clicked
       tableRow.remove();
       removeCart(cart.uniqueId);
       totalAmount(TotalAmount);
     });
 
-    // Append the remove button to the removeButtonCell
     removeButtonCell.appendChild(removeButton);
 
-    // Append the table cells to the table row in the desired order
     tableRow.appendChild(productNameCell);
     tableRow.appendChild(productPriceCell);
     tableRow.appendChild(removeButtonCell);
 
-    // Append the table row to the cart list
     cartList.appendChild(tableRow);
   });
 }
@@ -146,18 +136,15 @@ function addButtonClick(product, key) {
   console.log("carts", carts);
 }
 products.map((product) => {
-  // Create a card container div
   const card = document.createElement("div");
   card.classList.add("card");
   const timestamp = new Date().getTime();
 
-  // Random number between 0 and 9999 (you can adjust the range as needed)
   const randomNumber = Math.floor(Math.random() * 10000);
 
-  // Combine timestamp and random number to create a unique ID
   const uniqueId = `${timestamp}-${randomNumber}`;
 
-  // Create the title div
+ 
   const titleDiv = document.createElement("div");
   titleDiv.classList.add("title");
   titleDiv.style.backgroundImage = `url(${product.url})`;
@@ -165,20 +152,18 @@ products.map((product) => {
   titleHeading.textContent = product.name;
   titleDiv.appendChild(titleHeading);
 
-  // Create the content div
   const contentDiv = document.createElement("div");
   contentDiv.classList.add("content");
 
-  // Create the inner content div
+
   const innerContentDiv = document.createElement("div");
   innerContentDiv.classList.add("inner-content");
 
-  // Create the price div
   const priceDiv = document.createElement("div");
   priceDiv.classList.add("price");
   priceDiv.textContent = `₹ ${product.price}`;
 
-  // Create the "add" button div
+  
   const buttonDiv = document.createElement("div");
   buttonDiv.classList.add("button");
   buttonDiv.textContent = "Add";
@@ -187,17 +172,15 @@ products.map((product) => {
     addButtonClick(product, uniqueId);
   });
 
-  // Append elements to the inner content div
   innerContentDiv.appendChild(priceDiv);
   innerContentDiv.appendChild(buttonDiv);
 
-  // Append the title and inner content divs to the content div
   contentDiv.appendChild(titleDiv);
   contentDiv.appendChild(innerContentDiv);
 
-  // Append the content div to the card
+
   card.appendChild(contentDiv);
 
-  // Append the card to the product list container
+
   productList.appendChild(card);
 });
